0cdde13e4317655af71d219d34a37509cb3f5e87
Lilong <spikelee9624@gmail.com>
2023-04-06 09:37:54 +0800

    修改rem加载时间

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-      <script src="/utils/setRem.js" async></script>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<script
+				src="/utils/setRem.js"
+				async
+			></script>
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
