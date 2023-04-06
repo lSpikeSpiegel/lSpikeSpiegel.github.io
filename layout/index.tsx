1651f68710cece8137e01fc5cff28f1fc6da8bb8
Lilong <spikelee9624@gmail.com>
2023-04-06 10:23:50 +0800

    添加加载过程

--- layout/index.tsx
+++ layout/index.tsx
@@ -1,18 +1,57 @@
-import Header, { Option } from './header'
-import Footer from './footer'
+import Header, { Option } from "./header";
+import { Spin } from "antd";
+import Footer from "./footer";
+import { useState, useEffect } from "react";
 interface Props {
-  headOptions: Option[]
-  children: JSX.Element
+	headOptions: Option[];
+	children: JSX.Element;
 }
 
 export default function Layout(props: Props) {
-  return (
-    <>
-      <div className="w-screen h-screen flex flex-col overflow-x-hidden overflow-y-scroll">
-        <Header options={props.headOptions}></Header>
-        {props.children}
-        <Footer></Footer>
-      </div>
-    </>
-  )
+	const [loading, setLoading] = useState<boolean>(true);
+
+	useEffect(() => {
+		ckeckIsSetRem();
+	}, []);
+
+	const ckeckIsSetRem = () => {
+		if (window && window.__REM_SETTED__) {
+			setTimeout(() => {
+				setLoading(false);
+			}, 500);
+		} else {
+			setTimeout(() => {
+				ckeckIsSetRem();
+			}, 500);
+		}
+	};
+
+	const renderPage = () => {
+		if (loading) {
+			return <div className="w-screen h-screen"></div>;
+		} else {
+			return (
+				<>
+					<Header options={props.headOptions}></Header>
+					{props.children}
+					<Footer></Footer>
+				</>
+			);
+		}
+	};
+
+	return (
+		<>
+			<div className="w-screen h-screen flex flex-col overflow-x-hidden overflow-y-scroll">
+				<Spin
+					className="m-auto"
+					spinning={loading}
+					tip="Loading"
+					size="large"
+				>
+					{renderPage()}
+				</Spin>
+			</div>
+		</>
+	);
 }
--- public/utils/setRem.js
+++ public/utils/setRem.js
@@ -3,6 +3,7 @@ function setRem() {
 	const doc = document.documentElement;
 	const width = doc.clientWidth;
 	width && (doc.style.fontSize = width / 192 + "px");
+	window.__REM_SETTED__ = true;
 }
 // 改变窗口大小时重新设置 rem
 window.onload = setRem;
